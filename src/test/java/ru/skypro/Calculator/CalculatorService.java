56fd398d19cde02620cb352edd2533844a772626
madinakhatukhova <Madina-Khat@yandex.ru>
2025-01-14 20:57:36 +0300

    1

---
+++ src/test/java/ru/skypro/Calculator/CalculatorService.java
@@ -0,0 +1,12 @@
+package ru.skypro.Calculator;
+
+public interface CalculatorService {
+
+    Integer sum(Integer num, Integer num2);
+
+    Integer subtract(Integer num, Integer num2);
+
+    Integer multiply(Integer num, Integer num2);
+
+    Integer divide(Integer num, Integer num2);
+}
